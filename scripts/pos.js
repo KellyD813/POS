488482ba732c2ca4b07a8b9618b2c4b08b1c91c2
Kelly Devine <KellyD813@gmail.com>
2015-05-19 09:00:30 -0400

    moved render functions inside of click events

--- scripts/pos.js
+++ scripts/pos.js
@@ -1,4 +1,3 @@
-//LOCATING ELMS FROM HTML
 var $bill = $(".bill");
 var $subtotal = $(".subtotal");
 var $tax = $(".tax");
@@ -13,18 +12,15 @@ var $itemFButton = $("#FItem");
 var $itemGButton = $("#GItem");
 var $itemHButton = $("#HItem");
 
-//TOTALS
 var subtotalBill = 0;
 var taxBill = 0;
 var finalBill = 0;
 
-//CONSTRUCTOR FUNCTION (MENU ITEMS)
 var MenuItems = function(name, price) {
 	this.name = name;
 	this.price = price;
 };
 
-//CREATING MENU ITEMS
 var itemA = new MenuItems("Plain Toast", 5);
 var itemB = new MenuItems("Butter Toast", 6);
 var itemC = new MenuItems("Avocado Toast", 8);
@@ -34,7 +30,6 @@ var itemF = new MenuItems("Water", 3);
 var itemG = new MenuItems("Coffee", 4);
 var itemH = new MenuItems("Tea", 3);
 
-//FUNCTIONS TO RUN
 MenuItems.prototype.addToBill = function() {
 	this.li = $("<li>").text(this.name + " for $" + this.price);
 	$($bill).append(this.li);
@@ -59,48 +54,15 @@ MenuItems.prototype.render = function() {
 	this.updateBillDisplay(this.updateBillTotals());
 };
 
-// MORE SPECIFIC FUNCTIONS
-var renderA = function() {
-	itemA.render();
-};
+$($itemAButton).click(function() { itemA.render(); })
+$($itemBButton).click(function() { itemB.render(); })
+$($itemCButton).click(function() { itemC.render(); })
+$($itemDButton).click(function() { itemD.render(); })
+$($itemEButton).click(function() { itemE.render(); })
+$($itemFButton).click(function() { itemF.render(); })
+$($itemGButton).click(function() { itemG.render(); })
+$($itemHButton).click(function() { itemH.render(); })
 
-var renderB = function() {
-	itemB.render();
-};
-
-var renderC = function() {
-	itemC.render();
-};
-
-var renderD = function() {
-	itemD.render();
-};
-
-var renderE = function() {
-	itemE.render();
-};
-
-var renderF = function() {
-	itemF.render();
-};
-
-var renderG = function() {
-	itemG.render();
-};
-
-var renderH = function() {
-	itemH.render();
-};
-
-//CLICK EVENTS
-$($itemAButton).click(renderA);
-$($itemBButton).click(renderB);
-$($itemCButton).click(renderC);
-$($itemDButton).click(renderD);
-$($itemEButton).click(renderE);
-$($itemFButton).click(renderF);
-$($itemGButton).click(renderG);
-$($itemHButton).click(renderH);
 
 
 
